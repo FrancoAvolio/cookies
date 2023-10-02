389f8edeb172a26c9c833d07b0377fe9b4603560
FrancoAvolio <francoavolio@hotmail.com>
2023-10-02 15:35:17 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import handlebars from 'express-handlebars';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+
+const app = express();
+app.engine('handlebars', handlebars.engine());
+
+app.set('views', './src/views');
+
+app.set('view engine', 'handlebars');
+
+app.use('/', express.static('./src/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  session({
+    secret: 'franco',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.listen(8080, () => console.log('Ready'));
+
+app.get('/', (req, res) => {
+  res.render('cookies');
+});
+
+app.get('/getCookies', (req, res) => {
+  res.send(req.cookies);
+});
+
+app.post('/setCookies', (req, res) => {
+  const { nombre, valor } = req.body;
+  res.cookie(nombre, valor, { maxAge: 1000 * 10 }).send('Cookie creada');
+});
+
+app.get('/root', (req, res) => {
+  if (req.session?.nombre) {
+    const counter = req.session.counter++;
+    res.send(`hola ${req.session.nombre} te logueaste ${counter} veces`);
+  } else {
+    const nombre = req.query.nombre;
+    req.session.nombre = nombre;
+    req.session.counter = 1;
+    res.send('Te damos la bienvenida')
+  }
+});
